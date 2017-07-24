f132f88e3755aa465d0bfecb6311810481705fb4
BeasZ <eakkasitk@gmail.com>
2017-07-24 22:55:33 +0700

    save step1

--- src/pages/step-1/step-1.ts
+++ src/pages/step-1/step-1.ts
@@ -1,3 +1,4 @@
+import { RequestOptions, Http,Headers } from '@angular/http';
 import { Step2 } from './../step-2/step-2';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -23,13 +24,51 @@ export class Step1 {
   public doc_send_date: any = '';
   public doc_send_no:string = '' ;
   public user_complain_type_id:string = '';
+  public host = 'http://122.155.197.104/sysdamrongdham';
+  public auth = this.host+'/api/complaint/key_in';
+  public token: string;
  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public RequestOptions:RequestOptions,public http:Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Step1');
   }
+
+  saveData(){
+    let Complaint_data = {
+      complain_date:this.complain_date ,
+      recipient:this.recipient,
+      doc_receive_date: this.doc_receive_date,
+      doc_receive_no:this.doc_receive_no,
+      doc_send_date: this.doc_send_date,
+      doc_send_no:this.doc_send_no, 
+      user_complain_type_id: this.user_complain_type_id
+    }
+    
+    return new Promise((resolve, reject) => {
+
+      let headers = new Headers();
+      headers.append('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyaWQiOiIxIiwiZnVsbG5hbWUiOiJcdTBlMTlcdTBlMzJcdTBlMjJBZG1pbiBpc3RyYXRvciIsInVzZXJuYW1lIjoiYWRtaW5pc3RyYXRvciIsInBlcm1pc3Npb24iOlsiMjEiLCIyIiwiNSIsIjciLCIxMCIsIjExIiwiMTMiLCIxNCIsIjE1IiwiMTYiLCIxNyIsIjE4IiwiMjAiLCIxOSIsIjIyIiwiMjMiXSwiaWF0IjoxNTAwNzI2MDY5LCJleHAiOjE1MDA4MTI1Njl9.ldvVDx0ruP7-PGJrvnCZPCg8oewVqAHZJgHY85Vaw1c');
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+      let body = "complain_date=" + Complaint_data.complain_date + "&recipient=" + Complaint_data.recipient + "&doc_receive_date=" + Complaint_data.doc_receive_date + "&doc_receive_no=" + Complaint_data.doc_receive_no + "&doc_send_date=" + Complaint_data.doc_send_date + "&doc_send_no=" + Complaint_data.doc_send_no + "&user_complain_type_id=" + Complaint_data.user_complain_type_id + "&step=1";
+      let options = new RequestOptions({headers: headers});
+
+      this.http.post(this.auth, body, options).map(res => res).subscribe(
+        data => {
+          console.log(data['_body']);
+          this.navCtrl.push(Step2, {param1: data['_body']});
+        },
+        err => {
+          console.log('Http Error');
+          reject(err);
+        }
+      );
+
+    });
+    
+   }
 
 }
