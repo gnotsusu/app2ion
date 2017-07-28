41528f96950d3a7b152f4f4128bc43d85959c075
BeasZ <eakkasitk@gmail.com>
2017-07-28 23:40:40 +0700

    protection step1 , step2 and change date save

--- src/pages/step-1/step-1.ts
+++ src/pages/step-1/step-1.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Login } from './../login/login';
 import { Auth } from './../../providers/auth';
 import { Storage } from '@ionic/storage';
@@ -21,37 +21,37 @@ import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-an
 })
 export class Step1 {
   step2Page = Step2;
-  public complain_date:any = '' ;
-  public recipient:string = '';
+  public complain_date: any = '';
+  public recipient: string = '';
   public doc_receive_date: any = '';
-  public doc_receive_no:string = '' ;
+  public doc_receive_no: string = '';
   public doc_send_date: any = '';
-  public doc_send_no:string = '' ;
-  public user_complain_type_id:string = '';
+  public doc_send_no: string = '';
+  public user_complain_type_id: string = '';
   public host = 'http://122.155.197.104/sysdamrongdham';
-  public api_save = this.host+'/api/complaint/key_in';
+  public api_save = this.host + '/api/complaint/key_in';
   public token: any;
-  loading : any;
-  complainForm:FormGroup;
+  loading: any;
+  complainForm: FormGroup;
   submitAttempt: boolean = false;
-  
 
 
- 
+
+
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public RequestOptions:RequestOptions,
-    public http:Http,
-    public auth:Auth,
-    public loadCtrl:LoadingController,
-    public formBuilder:FormBuilder) {      
-      this.complainForm = formBuilder.group({
-        complain_date: ['',Validators.required],
-        recipient: ['',Validators.required]
+    public RequestOptions: RequestOptions,
+    public http: Http,
+    public auth: Auth,
+    public loadCtrl: LoadingController,
+    public formBuilder: FormBuilder) {
+    this.complainForm = formBuilder.group({
+      complain_date: ['', Validators.required],
+      recipient: ['', Validators.required]
     });
-    
+
   }
 
   ionViewDidLoad() {
@@ -60,96 +60,96 @@ export class Step1 {
     //this.token = this.auth.token;
   }
 
-  authen(){
+  authen() {
 
     this.showLoading();
 
     this.auth.isCheck().then((data) => {
-        this.token = data;
-        
-        return data;
-    }).then( (token:string) => {
-        return this.auth.isExpire(token);
-    }).then((isExpire:Boolean) => {
-      if (isExpire ) {
+      this.token = data;
+
+      return data;
+    }).then((token: string) => {
+      return this.auth.isExpire(token);
+    }).then((isExpire: Boolean) => {
+      if (isExpire) {
         this.loading.dismiss();
         this.navCtrl.pop(Login);
-      }else{
+      } else {
         this.loading.dismiss();
-      } 
+      }
     }).catch(err => {
       this.loading.dismiss();
       console.error(err.message);
     });
   }
 
-  showLoading(){
+  showLoading() {
     this.loading = this.loadCtrl.create({
-      content : 'กำลังยื่นยันตัวตน...'
+      content: 'กำลังยื่นยันตัวตน...'
     });
     this.loading.present();
   }
 
-  saveData(){
+  saveData() {
     this.submitAttempt = true;
     let Complaint_data = {
-      complain_date:this.complain_date ,
-      recipient:this.recipient,
+      complain_date: this.complain_date,
+      recipient: this.recipient,
       doc_receive_date: this.doc_receive_date,
-      doc_receive_no:this.doc_receive_no,
+      doc_receive_no: this.doc_receive_no,
       doc_send_date: this.doc_send_date,
-      doc_send_no:this.doc_send_no, 
+      doc_send_no: this.doc_send_no,
       user_complain_type_id: this.user_complain_type_id
     }
     let token_data = this.token;
 
-    let complain_date_ex:string[] = [];
-    let doc_receive_date_ex:string[] = [];
-    let doc_send_date_ex:string[] = [];
-if(this.complainForm.valid){
-    return new Promise((resolve, reject) => {      
-      let headers = new Headers();
-      headers.append('Authorization','Bearer '+ token_data);
-      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    let complain_date_ex: string[] = [];
+    let doc_receive_date_ex: string[] = [];
+    let doc_send_date_ex: string[] = [];
+    if (this.complainForm.valid) {
+      return new Promise((resolve, reject) => {
+        let headers = new Headers();
+        headers.append('Authorization', 'Bearer ' + token_data);
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-      let body = new URLSearchParams();
-      if(Complaint_data.complain_date != ""){
-        complain_date_ex = Complaint_data.complain_date.split("-");
-        body.set('complain_date',complain_date_ex[2]+"/"+complain_date_ex[1]+"/"+(parseInt(complain_date_ex[0])+543));
-      }
-
-      if(Complaint_data.doc_receive_date != ""){
-        doc_receive_date_ex = Complaint_data.doc_receive_date.split("-");
-        body.set('doc_receive_date',doc_receive_date_ex[2]+"/"+doc_receive_date_ex[1]+"/"+(parseInt(doc_receive_date_ex[0])+543));
-      }
-
-      if(Complaint_data.doc_send_date != ""){
-        doc_send_date_ex = Complaint_data.doc_send_date.split("-");
-        body.set('doc_send_date',doc_send_date_ex[2]+"/"+doc_send_date_ex[1]+"/"+(parseInt(doc_send_date_ex[0])+543));
-      }
-      
-      body.set('recipient',Complaint_data.recipient);      
-      body.set('doc_receive_no',Complaint_data.doc_receive_no);      
-      body.set('doc_send_no',Complaint_data.doc_send_no);
-      body.set('user_complain_type_id',Complaint_data.user_complain_type_id);
-      body.set('step','1');
-      let options = new RequestOptions({headers: headers});
-
-      this.http.post(this.api_save, body, options).map(res => res).subscribe(
-        data => {
-          console.log(data['_body']);
-          this.navCtrl.push(Step2, {param1: data['_body']});
-        },
-        err => {
-          console.log(token_data);
-          console.log('Http Error');
-          reject(err);
+        let body = new URLSearchParams();
+        if (Complaint_data.complain_date != "") {
+          complain_date_ex = Complaint_data.complain_date.split("-");
+          body.set('complain_date', complain_date_ex[2] + "/" + complain_date_ex[1] + "/" + (parseInt(complain_date_ex[0]) + 543) + " 00:00:00");
         }
-      );
 
-    });
-    
-   }
+        if (Complaint_data.doc_receive_date != "") {
+          doc_receive_date_ex = Complaint_data.doc_receive_date.split("-");
+          body.set('doc_receive_date', doc_receive_date_ex[2] + "/" + doc_receive_date_ex[1] + "/" + (parseInt(doc_receive_date_ex[0]) + 543) + " 00:00:00");
+        }
+
+        if (Complaint_data.doc_send_date != "") {
+          doc_send_date_ex = Complaint_data.doc_send_date.split("-");
+          body.set('doc_send_date', doc_send_date_ex[2] + "/" + doc_send_date_ex[1] + "/" + (parseInt(doc_send_date_ex[0]) + 543) + " 00:00:00");
+        }
+
+        body.set('recipient', Complaint_data.recipient);
+        body.set('doc_receive_no', Complaint_data.doc_receive_no);
+        body.set('doc_send_no', Complaint_data.doc_send_no);
+        body.set('user_complain_type_id', Complaint_data.user_complain_type_id);
+        body.set('step', '1');
+        let options = new RequestOptions({ headers: headers });
+
+        this.http.post(this.api_save, body, options).map(res => res).subscribe(
+          data => {
+            console.log(data['_body']);
+            this.navCtrl.push(Step2, { param1: data['_body'] });
+          },
+          err => {
+            console.log(token_data);
+            console.log('Http Error');
+            reject(err);
+          }
+        );
+
+      });
+
+    }
   }
 
 }
--- src/pages/step-2/step-2.ts
+++ src/pages/step-2/step-2.ts
@@ -1,20 +1,21 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Login } from './../login/login';
 import { Auth } from './../../providers/auth';
 import { SelectorComplaint } from './../../providers/selector-complaint';
 import { Complaint } from './../complaint/complaint';
 import { Step3 } from './../step-3/step-3';
-import { RequestOptions, Http, Headers} from '@angular/http';
+import { RequestOptions, Http, Headers, URLSearchParams } from '@angular/http';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 
 
-export class ComplaintType{
+export class ComplaintType {
 
-  id:number;
-  name:string;
+  id: number;
+  name: string;
 
-  public constructor(id:number, name:string){
+  public constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
   }
@@ -39,138 +40,155 @@ export class Step2 {
   public channel_id_sel: any;
   public subject_id_sel: any;
   public complain_type_id: any;
-  public complain_name:string = '';
+  public complain_name: string = '';
   public channel_id: any = '';
-  public subject_id:string = '' ;
+  public subject_id: string = '';
   public accused_type_id: any = '';
-  public accused_name:string = '' ;
-  public wish_detail:string = '';
+  public accused_name: string = '';
+  public wish_detail: string = '';
   public host = 'http://122.155.197.104/sysdamrongdham';
-  public api_keyin = this.host+'/api/complaint/key_in';
-  public complain_type = this.host+'/api/complaint/key_in';
+  public api_keyin = this.host + '/api/complaint/key_in';
+  public complain_type = this.host + '/api/complaint/key_in';
   public token: any;
-  public selectOptions:any;  
-  loading:any;
+  public selectOptions: any;
+  loading: any;
+  complainForm: FormGroup;
+  submitAttempt: boolean = false;
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public RequestOptions:RequestOptions,
-    public http:Http,
-    public SelectorComplaint:SelectorComplaint,
-    public auth:Auth,
-    public loadCtrl:LoadingController,
-    ) {
-    this.keyin_id = this.navParams.get('param1');; 
-  //   this.complain_type_id = [
-  //     new ComplaintType(1, 'key1'),
-  //     new ComplaintType(2, 'key2'),
-  //     new ComplaintType(3, 'key3'),
-  //     new ComplaintType(4, 'key4'),
-  // ];
-}
+    public RequestOptions: RequestOptions,
+    public http: Http,
+    public SelectorComplaint: SelectorComplaint,
+    public auth: Auth,
+    public loadCtrl: LoadingController,
+    public formBuilder: FormBuilder
+  ) {
+    this.keyin_id = this.navParams.get('param1');
+    console.log('keyin_id = ' + this.keyin_id);
+    this.complainForm = formBuilder.group({
+      complain_type_id: ['', Validators.required],
+      complain_name: ['', Validators.required],
+      channel_id: ['', Validators.required],
+      subject_id: ['', Validators.required],
+      accused_type_id: ['', Validators.required]
+    });
+  }
 
 
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Step2');
     this.authen();
-    this.SelectorComplaint.getComplaintTypeList().then((data)=>{
+    this.SelectorComplaint.getComplaintTypeList().then((data) => {
       this.complain_type_id_sel = data;
     });
 
-    this.SelectorComplaint.getChannelList().then((data)=>{
+    this.SelectorComplaint.getChannelList().then((data) => {
       this.channel_id_sel = data;
     });
 
-    this.SelectorComplaint.getSubjectList().then((data)=>{
+    this.SelectorComplaint.getSubjectList().then((data) => {
       this.subject_id_sel = data;
-    });    
-  //  this.getComplainType().then((data:ComplaintType[]) => {
-  //      this.complain_type_id = data;
-  //   });
+    });
+    //  this.getComplainType().then((data:ComplaintType[]) => {
+    //      this.complain_type_id = data;
+    //   });
 
-}
+  }
 
-authen(){
+  authen() {
 
     this.showLoading();
 
     this.auth.isCheck().then((data) => {
-        this.token = data;
-        
-        return data;
-    }).then( (token:string) => {
-        return this.auth.isExpire(token);
-    }).then((isExpire:Boolean) => {
-      if (isExpire ) {
+      this.token = data;
+
+      return data;
+    }).then((token: string) => {
+      return this.auth.isExpire(token);
+    }).then((isExpire: Boolean) => {
+      if (isExpire) {
         this.loading.dismiss();
         this.navCtrl.pop(Login);
-      }else{
+      } else {
         this.loading.dismiss();
-      } 
+      }
     }).catch(err => {
       this.loading.dismiss();
       console.error(err.message);
     });
   }
 
-  showLoading(){
+  showLoading() {
     this.loading = this.loadCtrl.create({
-      content : 'กำลังยื่นยันตัวตน...'
+      content: 'กำลังยื่นยันตัวตน...'
     });
     this.loading.present();
   }
 
-  getComplainType(){
-    return new Promise((resolve, reject)=>{
-      this.http.get(this.complain_type).map(res=> res.json()).subscribe((data) => {
-          let complainType:ComplaintType[];
-          for(let comp of data){
-              complainType.push(new ComplaintType(comp.id, comp.name));
-          }
+  getComplainType() {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.complain_type).map(res => res.json()).subscribe((data) => {
+        let complainType: ComplaintType[];
+        for (let comp of data) {
+          complainType.push(new ComplaintType(comp.id, comp.name));
+        }
 
-          resolve(complainType);
+        resolve(complainType);
       })
     });
   }
 
-  saveData(){
+  saveData() {
+    this.submitAttempt = true;
     let token_data = this.token;
     let keyin_id = this.keyin_id;
     let Complaint_data = {
-      complain_type_id:this.complain_type_id ,
-      complain_name:this.complain_name,
+      complain_type_id: this.complain_type_id,
+      complain_name: this.complain_name,
       channel_id: this.channel_id,
-      subject_id:this.subject_id,
+      subject_id: this.subject_id,
       accused_type_id: this.accused_type_id,
-      accused_name:this.accused_name, 
+      accused_name: this.accused_name,
       wish_detail: this.wish_detail
     }
-    console.log(Complaint_data);
-    
-    return new Promise((resolve, reject) => {
+    if (this.complainForm.valid) {
+      console.log(Complaint_data);
+      return new Promise((resolve, reject) => {
 
-      let headers = new Headers();
-      headers.append('Authorization','Bearer '+ token_data);
-      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        let headers = new Headers();
+        headers.append('Authorization', 'Bearer ' + token_data);
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        let body = new URLSearchParams();
+        body.set('complain_type_id', Complaint_data.complain_type_id);
+        body.set('complain_name', Complaint_data.complain_name);
+        body.set('channel_id', Complaint_data.channel_id);
+        body.set('subject_id', Complaint_data.subject_id);
+        body.set('accused_type_id', Complaint_data.accused_type_id);
+        body.set('accused_name', Complaint_data.accused_name);
+        body.set('wish_detail', Complaint_data.wish_detail);
+        body.set('step', '2');
+        body.set('keyin_id', keyin_id);
+        // let body = "complain_type_id=" + Complaint_data.complain_type_id + "&complain_name=" + Complaint_data.complain_name + "&channel_id=" + Complaint_data.channel_id + "&subject_id=" + Complaint_data.subject_id + "&accused_type_id=" + Complaint_data.accused_type_id + "&accused_name=" + Complaint_data.accused_name + "&wish_detail=" + Complaint_data.wish_detail + "&step=2&keyin_id=" + keyin_id;
+        let options = new RequestOptions({ headers: headers });
 
-      let body = "complain_type_id=" + Complaint_data.complain_type_id + "&complain_name=" + Complaint_data.complain_name + "&channel_id=" + Complaint_data.channel_id + "&subject_id=" + Complaint_data.subject_id + "&accused_type_id=" + Complaint_data.accused_type_id + "&accused_name=" + Complaint_data.accused_name + "&wish_detail=" + Complaint_data.wish_detail + "&step=2&keyin_id="+keyin_id;
-      let options = new RequestOptions({headers: headers});
+        this.http.put(this.api_keyin, body, options).map(res => res).subscribe(
+          data => {
+            console.log(data['_body']);
+            this.navCtrl.push(Step3, { param1: data['_body'] });
+          },
+          err => {
+            console.log(body);
+            console.log('Http Error');
+            reject(err);
+          }
+        );
 
-      this.http.put(this.api_keyin, body, options).map(res => res).subscribe(
-        data => {
-          console.log(data['_body']);
-          this.navCtrl.push(Step3, {param1: data['_body']});
-        },
-        err => {
-          console.log('Http Error');
-          reject(err);
-        }
-      );
+      });
+    }
 
-    });
-    
-   }
+  }
 
 
 
